ff0bc81fa1af4b96d755bb6f5b11aef0fdc5aad7
Shane Starcher <shanestarcher@gmail.com>
2019-10-14 15:03:22 -0700

    add additional parameters to app user profile

--- okta/models/app/AppUserProfile.py
+++ okta/models/app/AppUserProfile.py
@@ -2,7 +2,11 @@ class AppUserProfile:
 
     types = {
         'username': str,
-        'password': str
+        'password': str,
+        'email': str,
+        'emailType': str,
+        'givenName': str,
+        'familyName': str
     }
 
     def __init__(self):
@@ -10,3 +14,11 @@ class AppUserProfile:
         self.username = None  # str
 
         self.password = None  # str
+
+        self.email = None  # str
+
+        self.emailType = None  # str
+
+        self.givenName = None  # str
+
+        self.familyName = None  # str
